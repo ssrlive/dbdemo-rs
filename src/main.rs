0bfbebcae011b4103cfb6cfb6c6a9fdafb900bbf
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-01-31 15:10:58 +0800

    Basic authorization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,47 @@
+use base64::{engine::general_purpose, Engine as _};
 use rocket::{
-    catch, catchers, delete, get, post, put, routes,
+    catch, catchers, delete, get,
+    http::Status,
+    log, post, put, request, routes,
     serde::json::{serde_json::json, Value},
 };
+
+// Basic authentication, for example:
+// Basic base64(username:password)
+pub struct BasicAuth {
+    pub username: String,
+    pub password: String,
+}
+
+impl BasicAuth {
+    pub fn new(username: String, password: String) -> Self {
+        Self { username, password }
+    }
+
+    pub fn from_header(header: &str) -> Option<Self> {
+        let header = header.trim_start_matches("Basic ");
+        let decoded = general_purpose::STANDARD.decode(header).ok()?;
+        let decoded = String::from_utf8(decoded).ok()?;
+        let mut split = decoded.splitn(2, ':');
+        let username = split.next()?.to_string();
+        let password = split.next()?.to_string();
+        Some(Self::new(username, password))
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> request::FromRequest<'r> for BasicAuth {
+    type Error = ();
+
+    async fn from_request(request: &'r rocket::Request<'_>) -> request::Outcome<Self, Self::Error> {
+        if let Some(header) = request.headers().get_one("Authorization") {
+            if let Some(auth) = Self::from_header(header) {
+                return request::Outcome::Success(auth);
+            }
+        }
+        request::Outcome::Failure((Status::Unauthorized, ()))
+    }
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -9,7 +49,8 @@ fn index() -> &'static str {
 }
 
 #[get("/")]
-async fn get_products() -> Value {
+async fn get_products(auth: BasicAuth) -> Value {
+    println!("{}:{}", auth.username, auth.password);
     json!("Product::all()")
 }
 
